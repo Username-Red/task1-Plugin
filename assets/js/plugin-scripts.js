42154c13feb0a0a185ad8890de266811b7bffc32
Username-Red <ashkestrel@gmail.com>
2025-11-18 17:51:58 +0200

    experiment 1: remove third console log from js

--- assets/js/plugin-scripts.js
+++ assets/js/plugin-scripts.js
@@ -1,4 +1,3 @@
 // Frontend plugin JS
 console.log("My Custom Boilerplate Plugin JS loaded!");
 console.log("This is a change I made for the task 2 update!")
-console.log("If you are reading this, I now understand how to connect js to wordpress")
